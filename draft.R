eb0c8f6842712aa1ff3714bc07450610e3bed709
Aliama <Aliama-Bi@users.noreply.github.com>
2020-07-26 00:35:10 +1000

    `

---
+++ draft.R
@@ -0,0 +1,8 @@
+From topTable **tT**, we can access information:
+  
+  - `logFC: log2 fold change of TCMR/Normal,gives the value of the contrast`
+- `AveExpr: Average expression across all samples, in log2 CPM`
+- `t: logFC divided by its standard error`
+- `P.Value: Raw p-value (based on t) from test that logFC differs from 0`
+- `adj.P.Val: Benjamini-Hochberg false discovery rate adjusted p-value`
+- `B: log-odds that gene is DE (arguably less useful than the other columns)`
